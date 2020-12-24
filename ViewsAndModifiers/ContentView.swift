c69ca07924d77665f8a062887335ee9b699d16d7
Lime5005 <zlp5005@gmail.com>
2020-12-24 23:52:29 +0100

    a view property, computed, in a view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    //creat a view as a property: a computed property, not one that refers to another
+    var moto1: some View { Text("OK").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)}
+    var moto2 = Text("This is my project")
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            moto1
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            moto2
+                .foregroundColor(.red)
+        }
     }
 }
 
