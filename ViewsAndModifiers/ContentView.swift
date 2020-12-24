57174b68296103791af2b8ffce980eb615f6a4dd
Lime5005 <zlp5005@gmail.com>
2020-12-25 00:07:40 +0100

    composed view: a struct view in the main view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,16 +7,23 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    //creat a view as a property: a computed property, not one that refers to another
-    var moto1: some View { Text("OK").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)}
-    var moto2 = Text("This is my project")
+struct CapsuleText: View {
+    var text: String
     var body: some View {
-        VStack {
-            moto1
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            moto2
-                .foregroundColor(.red)
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            CapsuleText(text: "First")
+                .foregroundColor(.green)
+            CapsuleText(text: "Second")
         }
     }
 }
