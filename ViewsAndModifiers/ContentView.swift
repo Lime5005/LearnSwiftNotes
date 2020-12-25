530f5a3aa6529078278acdb5a111b0f1b804a6a7
Lime5005 <zlp5005@gmail.com>
2020-12-25 18:41:56 +0100

    row and columes: a grid modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,52 +7,40 @@
 
 import SwiftUI
 
-//Custom modifier:
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.red)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-//Wrap the Title modifier to an extension:
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-}
-
-//Create a new struct and use it in the old one:
-struct Watermark: ViewModifier {
-    var text: String
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.gray)
-                .padding(20)
-                .background(Color.yellow)
+//Generics: any kind of view content that conforms the View protocol
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content //this is a closure that returns a Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows ) { row in
+                HStack {
+                    ForEach(0 ..< columns ) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
-}
-
-//Wrap up so it apply the modifier function directly:
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping
+            (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
     }
 }
+
 
 struct ContentView: View {
     var body: some View {
-        Color.blue
-            .frame(width: 200, height: 200)
-            .watermarked(with: "Copy right reserved")
+        //r will repeat as much time as c needed: 3 columns, so row will repeat r0, r1 3 times as the columns grows
+        GridStack(rows: 2, columns: 3) { row, col in
+            Image(systemName: "\(row + col).circle")
+            Text("r\(row) c\(col)")
+        }
     }
 }
 
