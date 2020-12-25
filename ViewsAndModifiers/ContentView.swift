31156df8600f5993aefa9ca35480f4bc7551de1f
Lime5005 <zlp5005@gmail.com>
2020-12-25 18:53:49 +0100

    title modifier exercise

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,40 +7,28 @@
 
 import SwiftUI
 
-//Generics: any kind of view content that conforms the View protocol
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content //this is a closure that returns a Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0 ..< rows ) { row in
-                HStack {
-                    ForEach(0 ..< columns ) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
+struct prominentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .padding()
+            .background(Color.yellow)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping
-            (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
+}
+
+extension View {
+    func styleTitle() -> some View {
+        self.modifier(prominentTitle())
     }
 }
 
 
 struct ContentView: View {
     var body: some View {
-        //r will repeat as much time as c needed: 3 columns, so row will repeat r0, r1 3 times as the columns grows
-        GridStack(rows: 2, columns: 3) { row, col in
-            Image(systemName: "\(row + col).circle")
-            Text("r\(row) c\(col)")
-        }
+        Text("This is a prominent title")
+            .styleTitle()
     }
 }
 
